39564938960c9b7c52e79919d47b60f1ecb54b48
VladArzamasov <71029515+VladArzamasov@users.noreply.github.com>
2021-04-19 22:17:41 +0400

    lab 5

---
+++ AbstractSushi-Bar/SushiBarRestApi/Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using AbstractSushi_BarBusinessLogic.BindingModels;
+using AbstractSushi_BarBusinessLogic.BusinessLogics;
+using AbstractSushi_BarBusinessLogic.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SushiBarRestApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ClientLogic _logic;
+        public ClientController(ClientLogic logic)
+        {
+            _logic = logic;
+        }
+        [HttpGet]
+        public ClientViewModel Login(string login, string password) => _logic.Read(new
+ClientBindingModel
+        { Email = login, Password = password })?[0];
+        [HttpPost]
+        public void Register(ClientBindingModel model) => _logic.CreateOrUpdate(model);
+        [HttpPost]
+        public void UpdateData(ClientBindingModel model) => _logic.CreateOrUpdate(model);
+    }
+}
