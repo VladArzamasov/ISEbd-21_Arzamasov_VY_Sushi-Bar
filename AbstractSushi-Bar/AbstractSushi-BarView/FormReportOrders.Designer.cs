ff19f32c0babfcd3f3006d38e14ac9735e8823d5
VladArzamasov <71029515+VladArzamasov@users.noreply.github.com>
2021-05-25 11:41:18 +0400

    3

--- AbstractSushi-Bar/AbstractSushi-BarView/FormReportOrders.Designer.cs
+++ AbstractSushi-Bar/AbstractSushi-BarView/FormReportOrders.Designer.cs
@@ -28,19 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.dateTimePickerFrom = new System.Windows.Forms.DateTimePicker();
+            this.buttonSaveToPdf = new System.Windows.Forms.Button();
+            this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
+            this.buttonMake = new System.Windows.Forms.Button();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelTo);
+            this.panel1.Controls.Add(this.labelFrom);
+            this.panel1.Controls.Add(this.dateTimePickerFrom);
+            this.panel1.Controls.Add(this.buttonSaveToPdf);
+            this.panel1.Controls.Add(this.dateTimePickerTo);
+            this.panel1.Controls.Add(this.buttonMake);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1236, 49);
+            this.panel1.TabIndex = 7;
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Location = new System.Drawing.Point(248, 10);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(27, 20);
+            this.labelTo.TabIndex = 6;
+            this.labelTo.Text = "по";
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Location = new System.Drawing.Point(12, 10);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(20, 20);
+            this.labelFrom.TabIndex = 5;
+            this.labelFrom.Text = "C";
+            // 
+            // dateTimePickerFrom
+            // 
+            this.dateTimePickerFrom.Location = new System.Drawing.Point(42, 7);
+            this.dateTimePickerFrom.Name = "dateTimePickerFrom";
+            this.dateTimePickerFrom.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePickerFrom.TabIndex = 1;
+            // 
+            // buttonSaveToPdf
+            // 
+            this.buttonSaveToPdf.Location = new System.Drawing.Point(1076, 7);
+            this.buttonSaveToPdf.Name = "buttonSaveToPdf";
+            this.buttonSaveToPdf.Size = new System.Drawing.Size(147, 34);
+            this.buttonSaveToPdf.TabIndex = 4;
+            this.buttonSaveToPdf.Text = "Сохранить в Pdf";
+            this.buttonSaveToPdf.UseVisualStyleBackColor = true;
+            this.buttonSaveToPdf.Click += new System.EventHandler(this.ButtonSaveToPdf_Click);
+            // 
+            // dateTimePickerTo
+            // 
+            this.dateTimePickerTo.Location = new System.Drawing.Point(283, 7);
+            this.dateTimePickerTo.Name = "dateTimePickerTo";
+            this.dateTimePickerTo.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePickerTo.TabIndex = 2;
+            // 
+            // buttonMake
+            // 
+            this.buttonMake.Location = new System.Drawing.Point(535, 6);
+            this.buttonMake.Name = "buttonMake";
+            this.buttonMake.Size = new System.Drawing.Size(204, 37);
+            this.buttonMake.TabIndex = 3;
+            this.buttonMake.Text = "Сформировать";
+            this.buttonMake.UseVisualStyleBackColor = true;
+            this.buttonMake.Click += new System.EventHandler(this.ButtonMake_Click);
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "AbstractSushi_BarView.Report.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 89);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(1236, 483);
+            this.reportViewer.TabIndex = 8;
             // 
             // FormReportOrders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1239, 573);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.reportViewer);
             this.Name = "FormReportOrders";
             this.Text = "Заказы клиентов";
+            this.Click += new System.EventHandler(this.FormReportOrders_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFrom;
+        private System.Windows.Forms.Button buttonSaveToPdf;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTo;
+        private System.Windows.Forms.Button buttonMake;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
     }
 }
--- AbstractSushi-Bar/AbstractSushi-BarView/FormReportSushiComponents.Designer.cs
+++ AbstractSushi-Bar/AbstractSushi-BarView/FormReportSushiComponents.Designer.cs
@@ -28,81 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonSaveToExel = new System.Windows.Forms.Button();
-            this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.Sushi = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Component = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Col = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // buttonSaveToExel
-            // 
-            this.buttonSaveToExel.Location = new System.Drawing.Point(13, 13);
-            this.buttonSaveToExel.Name = "buttonSaveToExel";
-            this.buttonSaveToExel.Size = new System.Drawing.Size(195, 35);
-            this.buttonSaveToExel.TabIndex = 0;
-            this.buttonSaveToExel.Text = "Сохранить в Exel";
-            this.buttonSaveToExel.UseVisualStyleBackColor = true;
-            this.buttonSaveToExel.Click += new System.EventHandler(this.ButtonSaveToExcel_Click);
-            // 
-            // dataGridView
-            // 
-            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Sushi,
-            this.Component,
-            this.Col});
-            this.dataGridView.Location = new System.Drawing.Point(0, 55);
-            this.dataGridView.Name = "dataGridView";
-            this.dataGridView.RowHeadersWidth = 62;
-            this.dataGridView.RowTemplate.Height = 28;
-            this.dataGridView.Size = new System.Drawing.Size(1013, 553);
-            this.dataGridView.TabIndex = 1;
-            // 
-            // Sushi
-            // 
-            this.Sushi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Sushi.HeaderText = "Суши";
-            this.Sushi.MinimumWidth = 8;
-            this.Sushi.Name = "Sushi";
-            // 
-            // Component
-            // 
-            this.Component.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Component.HeaderText = "Компонент";
-            this.Component.MinimumWidth = 8;
-            this.Component.Name = "Component";
-            // 
-            // Col
-            // 
-            this.Col.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Col.HeaderText = "Количество";
-            this.Col.MinimumWidth = 8;
-            this.Col.Name = "Col";
-            // 
-            // FormReportSushiComponents
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1014, 606);
-            this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.buttonSaveToExel);
-            this.Name = "FormReportSushiComponents";
-            this.Text = "Изделия по компонентам";
-            this.Load += new System.EventHandler(this.FormReportSushiComponents_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "FormReportSushiComponents";
         }
 
         #endregion
-
-        private System.Windows.Forms.Button buttonSaveToExel;
-        private System.Windows.Forms.DataGridView dataGridView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Sushi;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Component;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Col;
     }
 }
