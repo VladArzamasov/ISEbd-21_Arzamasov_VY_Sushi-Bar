34eb753e4ce9c2b45fa060a4486eb9624a160a6a
VladArzamasov <71029515+VladArzamasov@users.noreply.github.com>
2021-05-25 11:32:50 +0400

    2

--- AbstractSushi-Bar/AbstractSushi-BarView/FormReportOrders.Designer.cs
+++ AbstractSushi-Bar/AbstractSushi-BarView/FormReportOrders.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormReportOrders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormReportOrders";
+            this.Name = "FormReportOrders";
+            this.Text = "Заказы клиентов";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
